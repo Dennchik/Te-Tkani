a4646d26984814b339763add3c547de3a834ae53
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-05-19 02:24:21 +0300

    19 May 2024

--- #src/js/layouts/layouts.js
+++ #src/js/layouts/layouts.js
@@ -5,23 +5,40 @@ const $ = {
 	bttns: document.querySelectorAll('.burger-button'),
 };
 // -----------------------------------------------------------------------------
-if (isMobile.any()) {
-	_loop($.bttns, 'burger-button', '_active');
+export function burgermenu() {
+	if (isMobile.any()) {
+		_loop($.bttns, 'burger-button', '_active');
+	}
+
+	function _loop(els, elClosest, md) {
+		for (let i = 0; i < els.length; i++) {
+			let item = els[i];
+			item.addEventListener('click', () => {
+				switch (true) {
+					case item.classList.contains(elClosest):
+						item.classList.toggle(md);
+						// $.openSide.classList.toggle('opened-menu');
+						break;
+					default:
+						item.closest(elClosest).classList.toggle(md);
+						break;
+				}
+			});
+		}
+	}
 }
 
-function _loop(els, elClosest, md) {
-	for (let i = 0; i < els.length; i++) {
-		let item = els[i];
-		item.addEventListener('click', () => {
-			switch (true) {
-				case item.classList.contains(elClosest):
-					item.classList.toggle(md);
-					// $.openSide.classList.toggle('opened-menu');
-					break;
-				default:
-					item.closest(elClosest).classList.toggle(md);
-					break;
-			}
-		});
-	}
+// -----------------------------------------------------------------------------
+export function showEMenu() {
+	let prevScrollpos = window.scrollY;
+	window.onscroll = function () {
+		const scrollmenu = document.querySelector('.page__buttom-menu');
+		let currentScrollPos = window.scrollY;
+		if (prevScrollpos > currentScrollPos) {
+			scrollmenu.style.bottom = "0px";
+		} else {
+			scrollmenu.style.bottom = "-65px";
+		}
+		prevScrollpos = currentScrollPos;
+	};
 }
--- #src/js/main.js
+++ #src/js/main.js
@@ -9,12 +9,13 @@ loaded('.preloader');
 // import returnToSavedPosition from './modules/return-position.js';
 // returnToSavedPosition();
 //* ----------------------------------------------------------------------------
-
+import { burgermenu, showEMenu } from "./layouts/layouts.js";
+burgermenu(); showEMenu();
 
 
 // -----------------------------------------------------------------------------
 
-import { } from "./layouts/layouts.js";
+
 //* ----------------------------------------------------------------------------
 console.log("%c РОССИЯ ", "background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;");
 //* ----------------------------------------------------------------------------
