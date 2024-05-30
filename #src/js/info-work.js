92ec39e46e9513e7a1143628dc4fac9f43a21055
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-05-31 00:04:05 +0300

    30 May 2024

--- #src/js/info-work.js
+++ #src/js/info-work.js
@@ -1,2 +1,6 @@
 import { anchorsSmoothScrolling } from "./modules/anchors-smooth-scrolling.js";
 anchorsSmoothScrolling();
+const sideBarButton = document.querySelector('.side-bar__menu');
+sideBarButton; addEventListener('click', () => {
+	sideBarButton.classList.toggle('_open');
+});
