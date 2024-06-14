a5c7b15f813941ae7f739cd17ad0dda4d044853a
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-06-15 00:37:00 +0300

    15 Jun 2024
    Production

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,3 +1,5 @@
+import { mainSlide } from "./modules/main-slide.js";
+mainSlide();
 import { newProductsSlide } from "./modules/new-products-slide.js";
 newProductsSlide();
 import { saleProductsSlide } from "./modules/sale-products-slide.js"; saleProductsSlide();
---
+++ #src/js/modules/main-slide.js
@@ -0,0 +1,41 @@
+import { swiperLayout } from "../layouts/swiper-layout";
+swiperLayout();
+//* import Swiper bundle with all modules installed 
+import Swiper from 'swiper/bundle';
+export function mainSlide(
+	mainslide = '.mainslide__body',
+	// nextEl = '.new-products__button-next',
+	// prevEl = '.new-products__button-prev',
+
+) {
+	if (mainslide) {
+		new Swiper(mainslide, {
+			autoplay: {
+				delay: 3500,
+				disableOnInteraction: true,
+			},
+			speed: 800,
+			spaceBetween: 10,
+			loop: true,
+			grabCursor: true,
+			autoHeight: true,
+			// mousewheel: true, 
+			slidesPerView: 1,
+			navigation: {
+				// nextEl: nextEl,
+				// prevEl: prevEl,
+			},
+			pagination: {
+				el: ".mainslide__dots",
+				clickable: true,
+
+			},
+
+		});
+	}
+}
+// -----------------------------------------------------------------------------
+
+
+
+
